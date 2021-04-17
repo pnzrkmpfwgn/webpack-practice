bb563a7bddf183061e9cec59c6de142c84c275d0
Ayberk Yigit <kurtknispel9@gmail.com>
2021-04-17 14:05:15 +0300

    envoirement variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,15 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'webpack-numbers.js',
-    library: {
-        name: 'webpackNumbers',
-        type: 'umd',
-      },
-      externals: {
-        lodash: {
-          commonjs: 'lodash',
-          commonjs2: 'lodash',
-          amd: 'lodash',
-          root: '_',
-        },
-      },
-  },
+module.exports = (env) => {
+  // Use env.<YOUR VARIABLE> here:
+  console.log('Goal: ', env.goal); // 'local'
+  console.log('Production: ', env.production); // true
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+  };
 };
